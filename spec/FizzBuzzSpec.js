a1abd1e0126c4e3519805e1383d4e4a0b0e2f9e0
EOSullivanBerlin <esully666@gmail.com>
2017-12-14 18:42:31 +0100

    Refractoring a few rough bits

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -7,21 +7,33 @@ describe('fizzbuzz', function(){
 
   describe('knows when a number is divisable', function() {
     it('by three', function() {
-      expect(javabuzz.isDivisableByThreeOrFive(3, 3)).toBe(true);
+      expect(javabuzz._isDivisableBy(3, 3)).toBe(true);
     });
 
     it('by five', function() {
-      expect(javabuzz.isDivisableByThreeOrFive(5, 5)).toBe(true);
+      expect(javabuzz._isDivisableBy(5, 5)).toBe(true);
+    });
+
+    it('by fifteen', function() {
+      expect(javabuzz.isDivisableByFifteen(15)).toBe(true);
     });
 
   });
 
   describe('knows when its not divisable', function() {
     it('by three', function() {
-      expect(javabuzz.isDivisableByThreeOrFive(1, 3)).toBe(false);
+      expect(javabuzz._isDivisableBy(1, 3)).toBe(false);
     });
-    
+
+    it('by five', function() {
+      expect(javabuzz._isDivisableBy(1, 5)).toBe(false);
+    });
+
+    it('by fifteen', function() {
+      expect(javabuzz.isDivisableByFifteen(3)).toBe(false);
+    });
   });
+
 
 
 
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,9 @@
 var JavaBuzz = function(){}
 
-JavaBuzz.prototype.isDivisableByThreeOrFive = function(number, divisor) {
-  return (number % divisor == 0)
+JavaBuzz.prototype._isDivisableBy = function(number, divisor) {
+  return (number % divisor === 0)
+};
+
+JavaBuzz.prototype.isDivisableByFifteen = function(number) {
+  return this._isDivisableBy(number, 15)
 };
