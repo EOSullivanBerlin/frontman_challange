dd5515a3e0fa97ad3e3dfba310bf9aca665481ec
EOSullivanBerlin <esully666@gmail.com>
2017-12-15 11:23:09 +0100

    Added power saving mode and mimimum temperture

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,6 +11,17 @@ describe('Thermostat', function() {
     expect(thermostat.CurrentTemperture()).toEqual(20);
   });
 
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i ++) {
+      thermostat.down()
+    }
+    expect(thermostat.CurrentTemperture()).toEqual(10);
+  });
+
+  it('has a power saving limit by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
   describe('increase', function() {
     it('the temp goes up by one when up is called', function() {
       thermostat.up();
@@ -24,5 +35,6 @@ describe('Thermostat', function() {
       expect(thermostat.CurrentTemperture()).toEqual(19);
     });
   });
+
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,9 @@
 'uses strict'
 
 var Thermostat = function(){
+  this.MINIMUM_TEMP = 10;
   this._temperture = 20;
+  this._powerSavingMode = true;
 };
 
 
@@ -13,6 +15,17 @@ Thermostat.prototype.up = function() {
   this._temperture += 1;
 };
 
+Thermostat.prototype.isMinimumTemp = function() {
+  return this.MINIMUM_TEMP === this._temperture;
+};
+
 Thermostat.prototype.down = function() {
-  this._temperture += -1;
+  if(this.isMinimumTemp()) {
+   return;
+  }
+  this._temperture -= 1;
+};
+
+Thermostat.prototype.isPowerSavingModeOn  = function() {
+  return this._powerSavingMode;
 };
